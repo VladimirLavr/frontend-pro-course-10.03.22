e7e905fd7eaf9d3061e861661743c83348d8731e
itsdorosh <andry.dorosh@gmail.com>
2022-03-29 23:42:48 +0300

    add examples of usage function for search

--- functions/callbacks.js
+++ functions/callbacks.js
@@ -1,19 +1,19 @@
-const server = function(data, callback) {
-  console.log(`server received ${data}.`);  
+const server = function (data, callback) {
+  console.log(`server received ${data}.`);
 
   setTimeout(() => {
     callback(`200 OK`);
   }, 3000);
 }
 
-const sendRequest = function(data, callback) {
+const sendRequest = function (data, callback) {
   const preparedData = JSON.stringify(data);
   server(preparedData, (response) => {
-    callback(`client received response: ${parseInt(response) === 200 ? "Everything is good" : 
-"Something went wrong :("}`);
+    callback(`client received response: ${parseInt(response) === 200 ? "Everything is good" :
+      "Something went wrong :("}`);
   });
 }
 
-sendRequest({name: "Andrii", age: 25}, (response) => {
+sendRequest({ name: "Andrii", age: 25 }, (response) => {
   console.log(response);
 });
--- objects/main.js
+++ objects/main.js
@@ -1,34 +1,65 @@
-// Objects
+// for auto clearing console while using nodemon
+// to use - type "npx nodemon [filename]"
 console.clear();
-const myObj = {
+
+const person = {
   name: "Andrii",
   age: 25,
   sayHi(name) {
-    return "hi " + name;
+    return `Hello, ${name}!`;
   }
 };
 
-const anotherObj = {name: "Andrii", age: 10, pull: undefined};
+// getting value of property - access expressions
+console.log(person.name);
+console.log(person["na" + "me"]);
 
-console.log(anotherObj.gender);
-console.log(anotherObj.pull);
-console.log(anotherObj["na" + "me"]);
+// calling a method seyHi
+console.log(person.sayHi("Mark"));
 
-console.log(myObj.sayHi("Mark"));
+// adding new property to object
+person.pull = undefined;
 
-console.log("gender" in anotherObj);
-console.log("pull" in anotherObj);
 
-anotherObj.gender = () => 'male';
-console.log(anotherObj.gender());
+// setted property vs. not existing property
+console.log(person.gender); // undefined
+console.log("gender" in person); // gender not even exists in object
+console.log(person.pull); // undefined
+console.log("pull" in person); // pull existing in object, but value setted to undefined
 
+// adding a method to object
+person.gender = () => 'male';
+console.log(person.gender());
+
+// creation of object using Object constructor
 const coolObj = new Object(); // {}
 console.log(coolObj);
 
-const greatObject = Object.create({foo: 5});
+// Creation of object based on another object
+const greatObject = Object.create({ foo: 5 });
 console.log(greatObject.foo);
 
+/* Preparation Homework */
 
-function greet(min, max) {
-  return {};
+// example of data
+const users = [{ email: "test@mail.com", username: "test", position: "web-developer" }];
+
+
+// function declaration expression for search function
+const search = function (searchObj) {
+  // your code goes here...
 }
+
+// defining test examples
+const testExampleOfSearchObject1 = { email: "test@mail.com" };
+const testExampleOfSearchObject2 = { position: "web-developer" };
+
+// calling a function for search and passing test examples
+const user1 = search(testExampleOfSearchObject1);
+console.log(user1);
+
+const user2 = search(testExampleOfSearchObject2);
+console.log(user2);
+
+const user3 = search({ somethingNotExisting: "foobar" });
+console.log(user3);
