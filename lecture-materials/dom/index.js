5ec91caf91b08df5b29fc1a217c43658ede1b084
itsdorosh <andry.dorosh@gmail.com>
2022-05-05 20:41:38 +0300

    add materials from lesson 16

--- lecture-materials/dom/index.js
+++ lecture-materials/dom/index.js
@@ -1,1 +1,37 @@
 console.log(`Hello, ${this.document.nodeName}`);
+
+// 'taxi-arrived': do something (e.g. show notification, play sound of arrival)
+// 'taxi-canceled': do something (e.g. retry)
+
+const p = document.querySelector('.simple-text');
+
+const testInput = document.getElementById('test');
+
+const handler = () => {
+  console.log('Hola!');
+};
+
+p.addEventListener('click', handler);
+
+setTimeout(() => p.removeEventListener('click', handler), 5000);
+
+console.dir(p);
+
+p.addEventListener('mousedown', (event) => {
+  console.log(event);
+});
+
+
+testInput.addEventListener('change', (event) => {
+  console.log(event);
+});
+
+
+document.addEventListener('keydown', (event) => {
+  null.toString(); // remove it 'cause it's will break the page
+})
+
+
+window.addEventListener('error', (error) => {
+  console.log(error);
+});
