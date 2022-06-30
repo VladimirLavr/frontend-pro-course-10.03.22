343650cf61b44438e033de5396a38bf31bf942bb
itsdorosh <andry.dorosh@gmail.com>
2022-06-30 20:15:11 +0300

    homework #23 - server part

---
+++ homeworks/hw23/server/todo.service.js
@@ -0,0 +1,37 @@
+import { v4 as uuid } from "uuid";
+
+const todos = [];
+
+export const getAllTodos = () => todos;
+
+export const createTodo = (todoConfig) => {
+  const createdTodo = {
+    id: uuid(),
+    title: todoConfig.title,
+    completed: todoConfig.completed || false,
+    createdAt: Date.now(),
+  };
+
+  todos.push(createdTodo);
+  return createdTodo;
+};
+
+export const deleteTodo = (id) => {
+  let itemIndex, foundElement;
+
+  foundElement = todos.find((todo, index) => {
+    if (todo.id === id) {
+      itemIndex = index;
+      return true;
+    }
+
+    return false;
+  });
+
+  if (foundElement) {
+    todos.splice(itemIndex, 1);
+    return true;
+  }
+
+  return false;
+};
